8cf14f54b02150afc91101b2ec437fdcea0b34e2
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-02-28 16:54:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,21 @@ otherwise: f(n-1)+C2
 
 Therefore, the time complexity = n
 """
+
+#another recursion method:
+"""
+def reverse_array_in_place(arr, start, end):
+    if start >= end:
+        return  # Base case: array is reversed
+    # Swap elements at start and end positions
+    else:
+        arr[start], arr[end] = arr[end], arr[start]
+        # Recurse with updated pointers
+        reverse_array_in_place(arr, start + 1, end - 1)
+        return arr
+
+# Example usage:
+array = [10, -2, 3, 4, 5]
+n = len(array)
+print(reverse_array_in_place(array, 0, n - 1))
+"""
